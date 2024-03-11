c52dfc68fb9cecff7bc358c18535fcdc6eb13fd0
Renato Pozzi <askides@proton.me>
2024-03-11 22:40:40 +0100

    feat(Button): add loading state

--- app/features/Admin/UI/Button.tsx
+++ app/features/Admin/UI/Button.tsx
@@ -2,27 +2,51 @@ import { forwardRef } from "react";
 import { tv, VariantProps } from "tailwind-variants";
 
 const styles = tv({
-  base: "w-full rounded-xl bg-black text-white font-semibold",
+  base: "rounded-xl bg-black text-white font-semibold disabled:bg-slate-700 disabled:cursor-not-allowed",
   variants: {
     size: {
-      sm: "h-10 text-sm",
-      md: "h-12 text-base",
-      lg: "h-14 text-lg",
+      sm: "h-10 text-sm px-4",
+      md: "h-12 text-base px-5",
+      lg: "h-14 text-lg px-6",
+    },
+    fluid: {
+      true: "w-full",
     },
   },
   defaultVariants: {
     size: "sm",
+    fluid: false,
   },
 });
 
+// Make a ThreeDots loader component
+export function ThreeDots() {
+  return (
+    <div className="flex space-x-1">
+      <div className="w-2 h-2 bg-white rounded-full animate-pulse" />
+      <div className="w-2 h-2 bg-white rounded-full animate-pulse" />
+      <div className="w-2 h-2 bg-white rounded-full animate-pulse" />
+    </div>
+  );
+}
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof styles> {}
+    VariantProps<typeof styles> {
+  loading?: boolean;
+}
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, ...props }, ref) => {
+  ({ className, loading = false, children, ...props }, ref) => {
     return (
-      <button className={styles({ class: className })} ref={ref} {...props} />
+      <button
+        className={styles({ class: className })}
+        ref={ref}
+        disabled={loading}
+        {...props}
+      >
+        {loading ? <ThreeDots /> : children}
+      </button>
     );
   }
 );
